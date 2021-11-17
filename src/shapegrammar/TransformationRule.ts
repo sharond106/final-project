e0c213550f8fed9fc6cbba9cac92d16e22244440
Sharon Dong <sharondo@seas.upenn.edu>
2021-11-17 16:28:01 -0500

    transformation rule

--- src/shapegrammar/TransformationRule.ts
+++ src/shapegrammar/TransformationRule.ts
@@ -3,20 +3,57 @@ import Shape from "./Shape";
 
 // Stores a list of resulting symbols and their transformations 
 class TransformationRule {
-  symbolResult: string;
-  translations: vec3[];
-  rotations: vec3[];
-  scales: vec3[];
+  tx: number;
+  ty: number;
+  tz: number;
+  rx: number;
+  ry: number;
+  rz: number;
+  sx: number;
+  sy: number;
+  sz: number;
 
-  constructor(symbols: string, t: vec3[], r: vec3[], s: vec3[]) {
-    this.symbolResult = symbols;
-    this.translations = t;
-    this.rotations = r;
-    this.scales = s;
+  constructor(tx: number, ty: number, tz: number, rx: number, ry: number, rz: number, sx: number, sy: number, sz: number) {
+    this.tx = tx;
+    this.ty = ty;
+    this.tz = tz;
+    this.rx = rx;
+    this.ry = ry;
+    this.rz = rz;
+    this.sx = sx;
+    this.sy = sy;
+    this.sz = sz;
   } 
 
-  transform(shape: Shape) : Shape {
-
+  transform(shape: Shape) : Shape{
+    if (this.tx != 0) {
+      shape.moveRight(this.tx);
+    }
+    if (this.ty != 0) {
+      shape.moveUp(this.ty);
+    }
+    if (this.tz != 0) {
+      shape.moveForward(this.tz);
+    }
+    if (this.rx != 0) {
+      shape.rotateAboutRight(this.tx);
+    }
+    if (this.ry != 0) {
+      shape.rotateAboutUp(this.ty);
+    }
+    if (this.rz != 0) {
+      shape.rotateAboutForward(this.tz);
+    }
+    if (this.sx != 0) {
+      shape.scaleX(this.tx);
+    }
+    if (this.sy != 0) {
+      shape.scaleY(this.ty);
+    }
+    if (this.sz != 0) {
+      shape.scaleZ(this.tz);
+    }
+    return shape;
   }
 }
 
